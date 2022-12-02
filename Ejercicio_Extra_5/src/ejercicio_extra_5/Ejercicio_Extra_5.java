75f6b72d13053c82f0b80c53056b0d6ea5a4b6a1
Daniel Roa <droa@apgslt.com>
2022-12-02 01:27:27 -0500

    EJ_Ext_1 V1

--- Ejercicio_Extra_5/src/ejercicio_extra_5/Ejercicio_Extra_5.java
+++ Ejercicio_Extra_5/src/ejercicio_extra_5/Ejercicio_Extra_5.java
@@ -1,8 +1,19 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+    EJERCICIO EXTRA 5:
+    Una obra social tiene tres clases de socios:
+    o Los socios tipo ‘A’ abonan una cuota mayor, pero tienen un 50% de descuento en
+    todos los tipos de tratamientos.
+    o Los socios tipo ‘B’ abonan una cuota moderada y tienen un 35% de descuento para
+    los mismos tratamientos que los socios del tipo A.
+    o Los socios que menos aportan, los de tipo ‘C’, no reciben descuentos sobre dichos
+    tratamientos.
+    o Solicite una letra (carácter) que representa la clase de un socio, y luego un valor
+    real que represente el costo del tratamiento (previo al descuento) y determine el
+    importe en efectivo a pagar por dicho socio.
  */
 package ejercicio_extra_5;
+
+import java.util.Scanner;
 
 /**
  *
@@ -15,6 +26,25 @@ public class Ejercicio_Extra_5 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Introduzca la Clase del Socio (A,B o C):");
+        char clase_socio=leer.next().charAt(0);
+        System.out.println("Ingrese el Costo del Tratamiento:");
+        float costo_tratamiento=leer.nextFloat();
+        
+        System.out.println("Calculando Importe a Pagar:");
+        switch(clase_socio){
+            case 'A':
+                System.out.println("Valor a Pagar por el Socio:"+costo_tratamiento*(1-0.50f));
+                break;
+            case 'B':
+            System.out.println("Valor a Pagar por el Socio:"+costo_tratamiento*(1-0.35f));
+                break;
+            case 'C':
+                System.out.println("Valor a Pagar por el Socio:"+costo_tratamiento);
+        }
+        
+        
     }
     
 }
