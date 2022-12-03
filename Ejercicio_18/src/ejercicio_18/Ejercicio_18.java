5e5c90116097507bd4dee069a91e7c2542fd28e5
Daniel Roa <droa@apgslt.com>
2022-12-02 19:37:28 -0500

    Inicio Ejercicio 18

---
+++ Ejercicio_18/src/ejercicio_18/Ejercicio_18.java
@@ -0,0 +1,23 @@
+/*
+    Ejercicio 18:
+    Realizar un programa que rellene un matriz de 4 x 4 de valores aleatorios y muestre la
+    traspuesta de la matriz. La matriz traspuesta de una matriz A se denota por B y se obtiene
+    cambiando sus filas por columnas (o viceversa).
+ */
+package ejercicio_18;
+
+/**
+ *
+ * @author droa
+ */
+public class Ejercicio_18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
