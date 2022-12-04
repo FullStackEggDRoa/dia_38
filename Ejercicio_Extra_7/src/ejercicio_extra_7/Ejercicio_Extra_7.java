3b8711f44f96eba27aa0dbabd52063a7116f2fca
Daniel Roa <droa@apgslt.com>
2022-12-03 20:09:36 -0500

    EJExt7-Dev-:(

--- Ejercicio_Extra_7/src/ejercicio_extra_7/Ejercicio_Extra_7.java
+++ Ejercicio_Extra_7/src/ejercicio_extra_7/Ejercicio_Extra_7.java
@@ -7,9 +7,12 @@
  */
 package ejercicio_extra_7;
 
+import java.util.Scanner;
+
 /**
  *
  * @author droa
+ * Versión con WHILE
  */
 public class Ejercicio_Extra_7 {
 
@@ -18,7 +21,42 @@ public class Ejercicio_Extra_7 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese la cantidad de número a ingresar:");
+        int n=leer.nextInt();
+        int n_max=0;
+        int n_min=0;
+        int n_sumatoria=0;
+        int num=0;
+        int n_cont=0;
+        boolean primerloop=true;
+        while(n_cont<n){
+            System.out.println("Ingresa el número "+(n_cont+1)+" (n>0): ");
+            num=leer.nextInt();
+            while(num<0){
+                System.out.println("Ingresa nuevamente el número "+(n_cont+1)+" (n>0): ");
+                num=leer.nextInt();
+            }
+            
+            //Sumatoria al promedio
+            n_sumatoria+=num;
+            //validación maximo
+            if(num>n_max){
+                n_max=num;
+            }
+            //validación mínimo
+            if(primerloop==true){
+                n_min=num;
+            }else{
+                if(num<n_min){
+                n_min=num;
+                }
+            }
+            n_cont++;
+        }
+        System.out.println("El Valor Máximo ingresado es: "+n_max);
+        System.out.println("El Valor Mínimo ingresado es: "+n_min);
+        System.out.println("El Valor Promedio de los números ingresados es: "+String.format("%.3g",((float)n_sumatoria/(float)n)));
     }
     
 }
