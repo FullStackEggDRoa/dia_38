57a317fceca28264e570d57ea581959ba93ea3a1
Daniel Roa <droa@apgslt.com>
2022-12-03 19:21:45 -0500

    Ejercicio Extra 7 - Inicio

---
+++ Ejercicio_Extra_7/src/ejercicio_extra_7/Ejercicio_Extra_7.java
@@ -0,0 +1,24 @@
+/*
+    EJERCICIO EXTRA 7
+    Realice un programa que calcule y visualice el valor máximo, el valor mínimo y el promedio
+    de n números (n>0). El valor de n se solicitará al principio del programa y los números
+    serán introducidos por el usuario. Realice dos versiones del programa, una usando el
+    bucle “while” y otra con el bucle “do - while”.
+ */
+package ejercicio_extra_7;
+
+/**
+ *
+ * @author droa
+ */
+public class Ejercicio_Extra_7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
